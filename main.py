ecede783b6cf0a5bd2a001931009110345e7b906
Usama Ashraf <uashraf588@gmail.com>
2016-02-09 20:56:49 +0500

    Document vector calculation bug fixed

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from stemming.porter2 import stem
 import collections
 import math
 import re
+from itertools import groupby
 
-def calculate_cosine_similarity(v1, v2):
-    euclidean = lambda x: math.sqrt(sum(i**2 for i in x))
-    return sum(list(map(lambda x,y: x * y, v1, v2))) / euclidean(v1) * euclidean(v2)
+def calculate_cosine_similarity(v1, v2, dm=0.0):
+    _denom = dm
+    if dm == 0.0:
+        euclidean = lambda x: math.sqrt(sum(j**2 for j in x))
+        _denom = euclidean(v2)
+    return sum(list(map(lambda x,y: x * y, v1, v2))) / _denom
 
 def create_term_doc_matrix(relative_dir, exclude_set, stop_words, min_frequency):
     """ Returns the matrix, the corpus document count and a dictionary holding document word counts. """
+
     search_dir = os.getcwd() + '/' + relative_dir
     if relative_dir not in os.listdir(os.getcwd()) or not os.path.isdir(search_dir):
         print('Sorry, directory ' + relative_dir + ' not found.')
@@ -26,14 +31,17 @@ def create_term_doc_matrix(relative_dir, exclude_set, stop_words, min_frequency)
         for filename in files:
 
             file_with_path = os.path.join(_dir, filename)
-            text = open(file_with_path, 'r').read().lower().replace('.', ' ')  # Down-casing, removing full-stops.
-            text = ''.join(ch for ch in text if ch not in exclude_set)         # Filtering out punctuation.
+            text = open(file_with_path, 'r').read().lower().replace('.', ' ')                     # Down-casing, removing full-stops.
+            text = ''.join(ch for ch in text if ch not in exclude_set)                            # Filtering out punctuation.
             terms = ' '.join(filter(lambda x: x.lower() not in stop_words, text.split())).split() # Removing the stop-list words.
             num_of_docs += 1
-            _document_word_counts[file_with_path] += len(terms)
+            terms.sort()
+            _document_word_counts[file_with_path] = {'word_count': len(terms),
+                                                     'magnitude': math.sqrt(sum([len(list(group)) for key, group in groupby(terms)]))}
 
             for _term_ in terms:
                 stemmed = stem(_term_)
+                _document_word_counts[file_with_path]['square_sum'] = terms.count(_term_)
                 if stemmed not in _parent_dictionary.keys():
                     _parent_dictionary[stemmed] = {}
                 child_dictionary = _parent_dictionary[stemmed]
@@ -61,10 +69,10 @@ print('Please enter queries in the exact specified format and with double quotes
       'If the given relative directory has not been indexed, it will first, preceding the search.')
 
 while True:
-    _input = input('\nEnter a query e.g "Learn life" 20_newsgroup : ')
+    _input = input('\nEnter a query e.g "Learn life" 20_newsgroup : ').lower()
     query = re.findall(r'"([^"]*)"', _input)[0]
     specified_dir = _input.split('"' + query + '"').pop().split()[0]
-    query = query.split()
+    query = [ stem(x) for x in query.split() ]
 
     if specified_dir not in DIR_RELEVANT_STORAGE.keys():
         result = create_term_doc_matrix(specified_dir, EXCLUDED, STOPWORD_LIST, MIN_REQUIRED_FREQUENCY)
@@ -74,35 +82,26 @@ while True:
 
     parent_dictionary, NUM_OF_DOCS, document_word_counts = DIR_RELEVANT_STORAGE[specified_dir]
     print('Searching... ! \n')
-    query_vector = []
-    idf_dict = {}
-    considerable_documents = []
-    for _term in query:
-        term = stem(_term)
-        idf = 0.0
-        if term in parent_dictionary.keys():
-            idf = 1 + math.log10(NUM_OF_DOCS / float(len(parent_dictionary[term])))
-            considerable_documents += parent_dictionary[term].keys()
-        idf_dict[term] = idf
-        tf = query.count(term) / float(len(query))
-        query_vector.append(tf * idf)
 
+    query_vector = []
     cosine_similarities = {}
     document_vectors = collections.defaultdict(list)
 
-    for _term in query:
-        term = stem(_term)
-        idf = idf_dict[term]
-        for doc in considerable_documents:
-            tf = 0.0
-            if term in parent_dictionary.keys() and doc in parent_dictionary[term].keys():
-                tf = parent_dictionary[term][doc] / float(document_word_counts[doc])
-            document_vectors[doc].append(tf * idf)
+    for term in query:
+        _tf1 = query.count(term) / float(len(query))
+        idf = 0.0
+        if (term in parent_dictionary.keys()) and (len(parent_dictionary[term].keys()) != 0):
+            idf = 1 + math.log10(NUM_OF_DOCS / float(len(parent_dictionary[term])))
+            for _doc in parent_dictionary[term].keys():
+                _tf2 = parent_dictionary[term][_doc] / float(document_word_counts[_doc]['word_count'])
+                document_vectors[_doc].append(idf * _tf2)
+        query_vector.append(_tf1 * idf)
 
     for doc in document_vectors.keys():
-        cosine_similarities[doc] = calculate_cosine_similarity(query_vector, document_vectors[doc])
+        cosine_similarities[doc] = calculate_cosine_similarity(query_vector, document_vectors[doc], dm=float(document_word_counts[doc]['magnitude']))
 
     top_sims = sorted(cosine_similarities.values(), reverse=True)
-    for cs in cosine_similarities.keys():
+    for i, cs in enumerate(cosine_similarities.keys()):
         if cosine_similarities[cs] in top_sims[:5]:
             print(cs)
+            if i == 4: break
